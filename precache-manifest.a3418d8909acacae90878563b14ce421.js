3a7004cd60b68923f62d0b13c2ab2d725c1af069
Shawnpal <shawnchen1915@gmail.com>
2019-07-09 22:17:29 -0700

    Updates

---
+++ precache-manifest.a3418d8909acacae90878563b14ce421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c393e400179fcfd510daaa88fdaed3c3",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "c1342cfebe9b4aa1f429",
+    "url": "/MyPortfolio/static/css/2.46fa11a7.chunk.css"
+  },
+  {
+    "revision": "7f15b5d7c23d8c2d9355",
+    "url": "/MyPortfolio/static/css/main.1e0fd5a1.chunk.css"
+  },
+  {
+    "revision": "c1342cfebe9b4aa1f429",
+    "url": "/MyPortfolio/static/js/2.f5370f0e.chunk.js"
+  },
+  {
+    "revision": "7f15b5d7c23d8c2d9355",
+    "url": "/MyPortfolio/static/js/main.df7ddcf3.chunk.js"
+  },
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  }
+]);
